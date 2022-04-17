9babdddff14f6d3417770840fffeeb453ee709dc
SmokingPC <smokingpc@gmail.com>
2022-04-17 21:23:57 +0800

    增加IOCTL_DISK 與 DeviceIoControl 項目

---
+++ src/SpcLibrary.DeviceIoControl/IOCTL_DISK.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpcLibrary.DeviceIoControl
+{
+    class IOCTL_DISK
+    {
+    }
+}
