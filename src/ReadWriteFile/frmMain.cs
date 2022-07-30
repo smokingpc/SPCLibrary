ae176799b8d999534c7c710b4c4b1a22b362c3fe
Roy Wang <smokingpc@gmail.com>
2022-07-31 01:21:37 +0800

    add ReadWriteFile example

---
+++ src/ReadWriteFile/frmMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWriteFile
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public void InitUI()
+        {
+            textBox1.Text = textBox2.Text = "";
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            InitUI();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = textBox2.Visible = true;
+            label3.Visible = label4.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = textBox2.Visible = false;
+            label3.Visible = label4.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (0 == textBox1.Text.Length)
+            {
+                MessageBox.Show("Please select file first!");
+                return;
+            }
+
+            if (true == radioButton1.Checked)
+            {
+                int read_len = 8;
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "All files(*.*)|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                //read first 8 bytes only
+                    byte[] data = DoReadFile(textBox1.Text, read_len);
+                    if (data != null)
+                    {
+                        textBox2.Text = data.ToHexString();
+                        MessageBox.Show($"Read {read_len} bytes from specified file done!");
+                    }
+                }
+            }
+            else if (true == radioButton2.Checked)
+            {
+                int read_len = label4.Text.Length;
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.Filter = "All files(*.*)|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(label4.Text);
+                    DoWriteFile(textBox1.Text, data);
+                    MessageBox.Show($"Write {data.Length} bytes to file done!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("radio button select error!");
+            }
+        }
+    }
+}
---
+++ src/ReadWriteFile/frmMain_Function.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpcLibrary.Common;
+using SpcLibrary.Win32API;
+
+namespace ReadWriteFile
+{
+    public partial class frmMain 
+    {
+        private byte[] DoReadFile(string filename, int read_len) 
+        {
+            using (CAutoHandle file = Kernel32.CreateFile(filename,
+                                            ACCESS_TYPE.GENERIC_READ,
+                                            FILE_SHARE_MODE.SHARE_READ | FILE_SHARE_MODE.SHARE_READ,
+                                            FILE_DISPOSITION.OPEN_EXISTING,
+                                            FILE_ATTR_AND_FLAG.NORMAL))
+
+            {
+                if (file.IsInvalid)
+                {
+                    MessageBox.Show($"OpenFile failed. LastError={Marshal.GetLastWin32Error()}");
+                    return null;
+                }
+
+                byte[] buffer = new byte[read_len];
+                UInt32 ret_size = 0;
+                bool ok = Kernel32.ReadFile(file, buffer, (UInt32)buffer.Length, ref ret_size, null);
+
+                if (!ok)
+                {
+                    MessageBox.Show($"ReadFile failed. LastError={Marshal.GetLastWin32Error()}");
+                    return null;
+                }
+
+                return buffer;
+            }
+        }
+        private void DoWriteFile(string filename, byte[] buffer) 
+        {
+            using (CAutoHandle file = Kernel32.CreateFile(filename,
+                                            ACCESS_TYPE.GENERIC_WRITE,
+                                            FILE_SHARE_MODE.SHARE_READ | FILE_SHARE_MODE.SHARE_READ,
+                                            FILE_DISPOSITION.CREATE_NEW,
+                                            FILE_ATTR_AND_FLAG.NORMAL))
+            {
+                UInt32 written_size = 0;
+
+                if (file.IsInvalid)
+                {
+                    MessageBox.Show($"OpenFile failed. LastError={Marshal.GetLastWin32Error()}");
+                    return;
+                }
+                bool ok = Kernel32.WriteFile(file, buffer, (UInt32)buffer.Length, ref written_size, null);
+
+                if(!ok)
+                    MessageBox.Show($"WriteFile failed. LastError={Marshal.GetLastWin32Error()}");
+            }
+        }
+    }
+
+    public static class BufferExt 
+    {
+        public static string ToHexString(this byte[] data)
+        {
+            string ret = "";
+
+            foreach (var item in data)
+            {
+                ret += string.Format("{0:X2} ", item);
+            }
+            return ret;
+        }
+    }
+}
